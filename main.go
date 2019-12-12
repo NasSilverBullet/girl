dc56500441055d6767896b8db9303c8a37707ad0
nas <nas.silver.bullet@gmail.com>
2019-12-12 14:56:02 +0900

    :+1: Add girl struct

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -15,27 +16,42 @@ func main() {
 	os.Exit(0)
 }
 
+type Girl struct {
+	URL         string
+	Method      string
+	Body        io.Reader
+	ShowReqBody bool
+	ShowResBody bool
+}
+
 func run() error {
-	req, err := http.NewRequest("GET", "https://google.com", nil)
+	g := Girl{
+		URL:         "https://google.com",
+		Method:      http.MethodGet,
+		ShowReqBody: true,
+		ShowResBody: false,
+	}
+
+	req, err := http.NewRequest(g.Method, g.URL, g.Body)
 	if err != nil {
 		return err
 	}
 
-	reqDump, err := httputil.DumpRequestOut(req, true)
+	reqDump, err := httputil.DumpRequestOut(req, g.ShowReqBody)
 	if err != nil {
 		return err
 	}
 
 	fmt.Println(string(reqDump))
 
-	client := new(http.Client)
+	client := &http.Client{}
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 
-	resDump, err := httputil.DumpResponse(res, true)
+	resDump, err := httputil.DumpResponse(res, g.ShowResBody)
 	if err != nil {
 		return err
 	}
